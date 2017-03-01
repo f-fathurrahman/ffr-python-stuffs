0d81381b6f971d43f150e0ff7810657b0f19c221
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-03-01 14:17:18 +0700

    Add a simple 3d plot example

---
+++ matplotlib/test_plot3d.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+t = np.arange(0,1.0,0.02)
+x = 0.5*t**2
+y = 0.5*(t-1.0)**2
+z = x + np.sin(y)
+
+ax.plot(x, y, z);
+
+plt.savefig("test_plot3d.png", dpi=300)
+plt.show()
