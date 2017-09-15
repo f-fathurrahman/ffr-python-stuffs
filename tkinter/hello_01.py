3768362992e49a267a0f65b0e0f2e76d00a5d469
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-09-15 13:55:05 +0700

    Test Arabic, not working in tkinter

---
+++ tkinter/hello_01.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import ttk
+
+from buck2uni import *
+import arabic_reshaper
+from bidi.algorithm import get_display
+
+root = Tk()
+ttk.Button(root, text=str1).grid()
+root.mainloop()
+
