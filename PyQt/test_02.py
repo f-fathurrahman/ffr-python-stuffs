f93ebb2a1c7b0f97374da79df1fb8e7e63128cb6
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-09-17 20:50:43 +0700

    Modify test_02, remove test if __name__ == '__main__'

--- PyQt/test_02.py
+++ PyQt/test_02.py
@@ -1,30 +1,19 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-"""
-ZetCode PyQt5 tutorial 
-
-In this example, we create a simple
-window in PyQt5.
-
-Author: Jan Bodnar
-Website: zetcode.com 
-Last edited: August 2017
-"""
 
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
 
-if __name__ == '__main__':
-    
-    app = QApplication(sys.argv)
+app = QApplication(sys.argv)
+w = QWidget()
+w.resize(250, 150)
+w.move(100, 300)
+w.setWindowTitle('Simple Window Example')
+w.show()
 
-    w = QWidget()
-    w.resize(250, 150)
-    w.move(300, 500)
-    w.setWindowTitle('Simple')
-    w.show()
-    
-    sys.exit(app.exec_())
+# why use sys.exit ?
+# sys.exit ensures a clean exit
+# The environment will be informed how the application ended.
+#sys.exit(app.exec_())
 
+app.exec_()
 
