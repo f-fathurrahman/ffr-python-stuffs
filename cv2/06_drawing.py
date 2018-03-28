c2832b932d37ffee0a5acc230faf1711501bc58c
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-03-28 11:43:57 +0700

    More shapes in 06 drawing

--- cv2/06_drawing.py
+++ cv2/06_drawing.py
@@ -6,7 +6,16 @@ import matplotlib.pyplot as plt
 img_bgr = np.zeros( (512,512,3), np.uint8 )
 
 # diagonal blue line with thickness 5px
-img_bgr = cv2.line( img_bgr, (0,0), (511,511), (255,0,0), 5)
+img_bgr = cv2.line( img_bgr, (0,0), (511,511), (255,0,0), 5 )
+
+# rectangle
+img_bgr = cv2.rectangle( img_bgr, (384,0), (510,128), (0,255,0), 3 )
+
+# circle
+img_bgr = cv2.circle( img_bgr, (447,63), 63, (0,128,128), -1 )
+
+# ellipse
+img_bgr = cv2.ellipse( img_bgr, (256,256), (100,50), 0, 0, 180, 255, -1 )
 
 img_rgb = cv2.cvtColor( img_bgr, cv2.COLOR_BGR2RGB )
 
