8a1a5a125882cdb859eecf72ca9bfb819091192b
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-05-18 15:29:25 +0700

    Still trying to converge Ni2

--- gpaw/07_Ni2/run_Ni2.py
+++ gpaw/07_Ni2/run_Ni2.py
@@ -1,19 +1,25 @@
 from ase import Atoms, Atom
 from gpaw import GPAW, FermiDirac
+from gpaw import Mixer, Davidson, PW
 
 a = 10.  # Size of unit cell (Angstrom)
 
 # Hydrogen atom:
 atoms = Atoms('Ni2',
-             positions=[(0.0,0.0,0.0),(1.5,0.0,0.0)],
-             cell=(a, a + 0.0001, a + 0.0002))  # Break cell symmetry
+             positions=[(0.0,0.0,0.0),(2.0,0.0,0.0)],
+             cell=(a, a, a),  # Break cell symmetry
+			 magmoms=[0.5,0.5],
+			 pbc=False)
 atoms.center()
+atoms.write("Ni2.xsf")
 
 # gpaw calculator:
-calc = GPAW(xc='PBE',
-            eigensolver='rmm-diis',  # This solver can parallelize over bands
-            occupations=FermiDirac(0.01), 
-            txt='-',
+calc = GPAW( mode=PW(),
+             xc='PBE',
+             eigensolver=Davidson(3),  # This solver can parallelize over bands
+             occupations=FermiDirac(0.01),
+             mixer=Mixer(0.02,1,100),
+             txt='LOG1'
             )
 
 atoms.set_calculator(calc)
