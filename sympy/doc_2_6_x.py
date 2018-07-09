f6cb2c412d80da26561e4ecc47d4f7b28e02098f
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-07-09 22:17:15 +0700

    cancel, apart, trigsimp

--- sympy/doc_2_6_x.py
+++ sympy/doc_2_6_x.py
@@ -74,7 +74,50 @@ print()
 pprint(eksp)
 eksp = collect(eksp, x)
 pprint(eksp)
-# use coeff
+# use `coeff`
 pprint(eksp.coeff(x,2))
+
+# using `cancel`
+
+eksp = (x**2 + 2*x + 1)/(x**2 + x)
+print()
+pprint(eksp)
+pprint(cancel(eksp))
+
+eksp = 1/x + (3*x/2 - 2)/(x - 4)
+print()
+pprint(eksp)
+pprint(cancel(eksp))
+
+eksp = (x*y**2 - 2*x*y*z + x*z**2 + y**2 - 2*y*z + z**2)/(x**2 - 1)
+print()
+pprint(eksp)
+eksp1 = cancel(eksp)
+pprint(eksp1)
+
+# `factor` also can be used to do the same thing
+eksp2 = factor(eksp)
+print()
+pprint(eksp2)
+
+print()
+# check that eksp1 and eksp2 is the same
+pprint(expand(eksp1 - eksp2))
+
+
+# `apart` performs partial fraction decomposition on a rational function
+eksp = (4*x**3 + 21*x**2 + 10*x + 12)/(x**4 + 5*x**3 + 5*x**2 + 4*x)
+print()
+pprint(eksp)
+print("\nDoing partial fraction decomposition")
+pprint(apart(eksp))
+
+#
+eksp = sin(x)**4 - 2*cos(x)**2 * sin(x)**2 + cos(x)**4
+print()
+print("\nOriginal expression:")
+pprint(eksp)
+print("\nAfter trigsimp:")
+pprint(trigsimp(eksp))
 
 
