f50441233e9ac05c730af5cae0f171a26daa50ad
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-07-10 11:32:01 +0700

    Some stuffs again ffrom doc_2_6_x, some topics may be too advanced, better test in different file

--- sympy/doc_2_6_x.py
+++ sympy/doc_2_6_x.py
@@ -113,11 +113,154 @@ print("\nDoing partial fraction decomposition")
 pprint(apart(eksp))
 
 #
+# trigonometric functions 
+#
+
+# `trigsimp`
+
 eksp = sin(x)**4 - 2*cos(x)**2 * sin(x)**2 + cos(x)**4
 print()
 print("\nOriginal expression:")
 pprint(eksp)
 print("\nAfter trigsimp:")
 pprint(trigsimp(eksp))
+
+
+eksp = sin(x)*tan(x)/sec(x)
+print()
+print("\nOriginal expression:")
+pprint(eksp)
+print("\nAfter trigsimp:")
+pprint(trigsimp(eksp))
+
+
+eksp = cosh(x)**2 + sinh(x)**2
+print()
+print("\nOriginal expression:")
+pprint(eksp)
+print("\nAfter trigsimp:")
+pprint(trigsimp(eksp))
+
+
+eksp = sinh(x)/tanh(x)
+print()
+print("\nOriginal expression:")
+pprint(eksp)
+print("\nAfter trigsimp:")
+pprint(trigsimp(eksp))
+
+# `expand_trig`: apply the sum or double angle identities
+
+eksp = sin(x + y)
+print()
+print("\nOriginal expression:")
+pprint(eksp)
+print("\nAfter expand_trig:")
+pprint(expand_trig(eksp))
+
+eksp = tan(2*x)
+print()
+print("\nOriginal expression:")
+pprint(eksp)
+print("\nAfter expand_trig:")
+pprint(expand_trig(eksp))
+
+eksp = sin(2*x)
+print()
+print("\nOriginal expression:")
+pprint(eksp)
+print("\nAfter expand_trig:")
+pprint(expand_trig(eksp))
+
+# trigsimp can be used to reverse these expansions
+
+#
+# Expression involving powers
+#
+
+# Introduce new symbol with assumption
+x, y = symbols("x y", positive=True)
+a, b = symbols("a b", real=True)
+z, t, c = symbols("z t c")
+
+# sqrt(x) is just a shortcut to x**Rational(1,2)
+eq = sqrt(x) == x**Rational(1/2)
+print()
+pprint(eq)
+
+# `powsimp`
+eksp = x**a * x**b
+print()
+pprint(eksp)
+pprint(powsimp(eksp))
+
+eksp = x**a * y**a
+pprint(eksp)
+pprint(powsimp(eksp))
+
+#
+eksp = t**c * z**c
+pprint(eksp)
+pprint(powsimp(eksp))
+# force it to do the simplification
+pprint(powsimp(eksp,force=True))
+
+
+eksp = (z*t)**2
+pprint(eksp)
+
+eksp = sqrt(x*y)
+pprint(eksp)
+
+
+# `expand_power_exp` and `expand_power_base`
+# TODO
+
+
+# `powdenest`
+# TODO
+
+#
+# Exponentials and logarithm
+#
+
+# `expand_log`
+# TODO
+
+# `logcombine`
+# TODO
+
+
+
+#
+# Special functions
+#
+
+# TODO
+
+
+# `rewrite`
+eksp = tan(x)
+print()
+print("\nRewriting tan(x) as sin(x)")
+pprint(eksp.rewrite(sin))
+
+# `expand_func`: expand special functions in terms of some identities
+
+pprint(expand_func(gamma(x+3)))
+
+pprint(expand_func(besselj(3,x)))
+
+# `hyperexpand`
+# TODO
+
+# `combsimb`
+# TODO
+
+
+# Continued fractions
+# TODO
+
+
 
 
