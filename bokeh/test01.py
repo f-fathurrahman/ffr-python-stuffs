7df263950060b292155ffe436f42b6b83e72272f
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-07-11 12:04:47 +0700

    Exclude html output in bokeh/

--- bokeh/test01.py
+++ bokeh/test01.py
@@ -1,7 +1,7 @@
 import bokeh
 import numpy as np
 import bokeh.plotting as bp
-bp.output_file("bokeh1.html", mode="inline")
+bp.output_file("TEMP_bokeh1.html", mode="inline")
 x = np.linspace(0,2*np.pi,1024)
 y = np.cos(x)*np.sin(2*x)
 fig = bp.figure()
