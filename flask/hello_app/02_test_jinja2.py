9863adfc4788e9bd4eadea877955885814002923
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-07-20 22:32:16 +0700

    Rename and test printing variable and class

--- flask/hello_app/02_test_jinja2.py
+++ flask/hello_app/02_test_jinja2.py
@@ -1,5 +1,27 @@
+from __future__ import print_function
+
 from jinja2 import Template
 
 t = Template("{{ variable }}")
+
+# Using built-in types
+
+print( t.render(variable="This is a string") )
+
+print( t.render(variable=100) )
+
+
+# Using custom classes instances
+
+class MyClass(object):
+    
+    def __str__(self):
+        return "__str__"
+    
+    def __repr__(self):
+        return "__repr__"
+
+print( t.render( variable=MyClass() ) )
+
 
 
