8c8ca19ff5d5803af8fb57c57cf8b8e23d812b92
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-07-20 22:14:34 +0700

    Add hello_app

---
+++ flask/hello_app/02_test_jinja2.py
@@ -0,0 +1,5 @@
+from jinja2 import Template
+
+t = Template("{{ variable }}")
+
+
---
+++ flask/hello_app/test_jinja2.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+
+from jinja2 import Template
+
+x = """
+<p>Uncle Scrooge nephews</p>
+<ul>
+{% for i in my_list %}
+<li>{{ i }}</li>
+{% endfor %}
+</ul>
+"""
+
+template = Template(x)
+
+print( template.render(my_list=['Huey', 'Dewey', 'Louie']) )
+
+
