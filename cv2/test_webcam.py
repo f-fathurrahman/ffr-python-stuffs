059398e4132231f1736fe85d14c33733274690ba
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-03-10 21:17:02 +0700

    Test scaling webcam view

--- cv2/test_webcam.py
+++ cv2/test_webcam.py
@@ -6,7 +6,7 @@ if not cap.isOpened():
 
 while True:
     ret, frame = cap.read()
-    frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+    frame = cv2.resize(frame, None, fx=2.5, fy=2.5, interpolation=cv2.INTER_AREA)
     cv2.imshow('Input', frame)
     c = cv2.waitKey(1)
     if c == 27:
