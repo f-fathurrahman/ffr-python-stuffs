ab40925cd1407138bafd9abebcdb24241000d2ff
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-07-30 11:20:09 +0700

    Testing Flask from virtual env

---
+++ env_flask/hello/main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from config import DevConfig
+
+app = Flask(__name__)
+# Load configuration
+app.config.from_object(DevConfig)
+
+
+@app.route("/")
+def home():
+    return "<h1>Hello world</h1>"
+
+if __name__ == "__main__":
+    app.run()
+
+
