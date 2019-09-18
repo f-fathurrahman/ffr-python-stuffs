566576f69b77ceb7dc32412c152d82649b230bae
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-09-18 22:16:01 +0700

    Minor string stuffs

--- matplotlib/mplot3d/02_2d_on_3d.py
+++ matplotlib/mplot3d/02_2d_on_3d.py
@@ -3,24 +3,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # use ax.plot's zdir keyword
-
 fig = plt.figure()
-ax = fig.gca(projection='3d')
+ax = fig.gca(projection="3d")
 
 # sin curve using x and y axes
 x = np.linspace(0, 1, 100)
 y = np.sin(x*2*np.pi)/2 + 0.5
-ax.plot(x, y, zs=0, zdir='z', label='curve in (x,y)', marker='o', linewidth=2)
+ax.plot(x, y, zs=0, zdir="z", label="curve in (x,y)", marker="o", linewidth=2)
 
 ax.legend()
 ax.set_xlim(0, 1)
 ax.set_ylim(0, 1)
 ax.set_zlim(0, 1)
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
+ax.set_zlabel("Z")
 
 ax.view_init(elev=20.0, azim=-35)
 
-plt.show()
-plt.savefig('02.png', dpi=300)
+plt.savefig("TEMP_02.png", dpi=300)
