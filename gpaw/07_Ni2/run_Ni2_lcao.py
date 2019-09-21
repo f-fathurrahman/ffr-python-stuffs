ee3c865bc8969e5bc4e404715f8931f44774616e
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-09-21 23:32:14 +0700

    Test Ni2 LCAO, need small linear mix parameter

---
+++ gpaw/07_Ni2/run_Ni2_lcao.py
@@ -0,0 +1,29 @@
+from ase import Atoms
+from gpaw import GPAW, FermiDirac
+from gpaw import MixerDif, PW
+
+a = 10.0  # Size of unit cell (Angstrom)
+
+atoms = Atoms("Ni2",
+             positions=[(0.0,0.0,0.0),
+                        (2.0,0.0,0.0)],
+             cell=(a, a, a),
+             magmoms=[1.7, 1.7],
+             pbc=False)
+atoms.center(vacuum=4.0)
+atoms.write("Ni2.xsf")
+
+# gpaw calculator:
+calc = GPAW( mode="lcao",
+             basis="dzp",
+             xc="PBE",
+             occupations=FermiDirac(0.01),
+             mixer=MixerDif(0.2, 5, 100),
+             txt="-"
+)
+
+atoms.set_calculator(calc)
+
+e1 = atoms.get_potential_energy()
+calc.write("Ni2_lcao.gpw")
+
