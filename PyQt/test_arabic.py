2e3d48612a58aa37fe36cd993b06f2616a347c80
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-11-30 14:25:02 +0700

    Renaming to test_arabic

---
+++ PyQt/test_arabic.py
@@ -0,0 +1,32 @@
+import sys
+from PyQt5.QtWidgets import QWidget, QPushButton, QApplication
+from buck2uni import *
+
+class MyWindow(QWidget):
+
+    def __init__(self, btn_label="Hello"):
+        
+        QWidget.__init__(self)
+        
+        self.setGeometry(300, 300, 200, 200)
+        self.setWindowTitle('Demo windows')
+        
+        quit = QPushButton(btn_label, self)
+        quit.setGeometry(10, 10, 70, 40)
+        
+        font = quit.font()
+
+        font.setPointSize(16)
+        quit.setFont(font)
+        quit.clicked.connect(app.quit)
+
+
+app = QApplication(sys.argv)
+
+window = MyWindow(btn_label=transliterateString("bisomi"))
+window.show()
+
+window2 = MyWindow(btn_label="Hello World")
+window2.show()
+
+sys.exit(app.exec_())
