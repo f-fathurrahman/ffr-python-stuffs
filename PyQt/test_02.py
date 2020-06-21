7bb39ca138982623d4c01482379d00a13fca8df7
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-06-22 03:03:19 +0700

    Add title

--- PyQt/test_02.py
+++ PyQt/test_02.py
@@ -7,13 +7,11 @@ app = QApplication(sys.argv)
 w = QWidget()
 w.resize(250, 150)
 w.move(100, 300)
-w.setWindowTitle('Simple Window Example')
+w.setWindowTitle("Simple Window Example")
 w.show()
 
 # why use sys.exit ?
 # sys.exit ensures a clean exit
 # The environment will be informed how the application ended.
-#sys.exit(app.exec_())
-
-app.exec_()
+sys.exit( app.exec_() )
 
