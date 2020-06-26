e950f05e618df2a0f79b0b45e2d64cf9b6a88c2c
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-06-26 09:23:27 +0700

    Print score and modle parameters

--- sklearn/13_perceptron.py
+++ sklearn/13_perceptron.py
@@ -6,9 +6,15 @@ iris = load_iris()
 X = iris.data[:,(2,3)] # petal length, petal width
 y = (iris.target == 0).astype(np.int)
 
+# Classifier based on Perceptron
 model = Perceptron()
 model.fit(X, y)
-print(model)
+
+parameters = model.get_params()
+for k,v in parameters.items():
+    print(k, " : ", v)
+
+print(model.score(X, y))
 
 y_pred = model.predict([[2.0, 0.5]])
 print(y_pred)
