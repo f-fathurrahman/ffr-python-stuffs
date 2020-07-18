d00c9a54520f982f8f814b7935de7505570aaeae
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-07-18 14:20:09 +0700

    Test Adam optimizer

---
+++ pytorch/06_simple_model.py
@@ -0,0 +1,46 @@
+# Using optim.SGD
+
+import torch
+import torch.optim as optim
+
+def model(t_u, w, b):
+    return w*t_u + b
+
+def loss_fn(t_p, t_c):
+    s = (t_p - t_c)**2
+    return s.mean()
+
+# Data
+t_c = [0.5, 14.0, 15.0, 28.0, 11.0, 8.0, 3.0, -4.0, 6.0, 13.0, 21.0]
+t_u = [35.7, 55.9, 58.2, 81.9, 56.3, 48.9, 33.9, 21.8, 48.4, 60.4, 68.4]
+t_c = torch.tensor(t_c)
+t_u = torch.tensor(t_u)
+
+
+def training_loop(Nepochs, optimizer, params, t_u, t_c):
+    #
+    for epoch in range(1,Nepochs+1):
+        #
+        t_p = model(t_u, *params)
+        loss = loss_fn(t_p, t_c)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        #        
+        if epoch % 500 == 0:
+            print("Epoch %8d, Loss %18.10f " % (epoch, float(loss)))
+    return params
+
+# Using normalized/scaled input
+print("\nUsing Adam\n")
+params = torch.tensor([1.0, 0.0], requires_grad=True)
+learning_rate = 1e-1
+optimizer = optim.Adam([params], lr=learning_rate)
+params = training_loop(
+    Nepochs=2000,
+    optimizer=optimizer,
+    params=params,
+    t_u=t_u, # using original t_u
+    t_c=t_c
+)
+print("params = ", params)
