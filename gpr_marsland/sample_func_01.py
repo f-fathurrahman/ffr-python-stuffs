f7180ead3262edbd456edbfc6dade53989dc75cf
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-08-31 00:28:01 +0700

    Larger Nsamples and Nbins

--- gpr_marsland/sample_func_01.py
+++ gpr_marsland/sample_func_01.py
@@ -21,21 +21,21 @@ plt.savefig("IMG_sample_func_01_a.pdf")
 
 
 # Sample random parameters, now with big number of samples
-Nsample = 10000
+Nsample = 50000
 a_set = 1.0 + 0.25*np.random.randn(Nsample)
 x = 1.0 # Fixed x
 
 plt.clf()
-Nbins = 25
+Nbins = 40
 #f1 = np.exp(-a_set*x**2)
 f1 = np.exp(-a_set*x**2)
 c = plt.hist(f1, bins=Nbins)
 plt.xlim(0.0, 1.0)
 plt.savefig("IMG_sample_func_01_b.pdf")
 
-#p = c[0]/np.max(c[0])
-#plt.clf()
-#plt.plot(c[1][:Nbins], p)
-#plt.xlabel("f(x)")
-#plt.ylabel("Pr(f(x))")
-#plt.savefig("IMG_sample_func_01_c.pdf")
+p = c[0]/np.max(c[0])
+plt.clf()
+plt.plot(c[1][:Nbins], p)
+plt.xlabel("f(x)")
+plt.ylabel("Pr(f(x))")
+plt.savefig("IMG_sample_func_01_c.pdf")
