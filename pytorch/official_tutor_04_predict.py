45373bb8f61e5c13ca889d3f3b64da8037c10621
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-10-17 18:43:53 +0700

    Separate definition of Net, test retraining

--- pytorch/official_tutor_04_predict.py
+++ pytorch/official_tutor_04_predict.py
@@ -5,26 +5,7 @@ import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
 
-class Net(nn.Module):
-    def __init__(self):
-        N = 10
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, N, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(N, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-
+from my_net import Net
 
 # The output of torchvision datasets are PILImage images with range [0,1]
 # We transform them to Tensors of normalized range [-1,1].
@@ -56,24 +37,12 @@ classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 
-import matplotlib.pyplot as plt
-import numpy as np
-
-def imshow(img):
-    img = img/2 + 0.5 # transform back
-    npimg = img.numpy()
-    plt.imshow( np.transpose(npimg, (1,2,0)) )
-    plt.savefig("IMG_some_images.png", dpi=150)
-
 # get some random training images
 dataiter = iter(trainloader)
 images, labels = dataiter.next()
 
-# show images
-imshow(torchvision.utils.make_grid(images))
 # print labels
 print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
-
 
 PATH = './cifar_net.pth'
 
--- pytorch/official_tutor_04_train.py
+++ pytorch/official_tutor_04_train.py
@@ -2,9 +2,10 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 
+torch.manual_seed(1234) # for reproducibility
+
 # The output of torchvision datasets are PILImage images with range [0,1]
 # We transform them to Tensors of normalized range [-1,1].
-
 transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize( (0.5,0.5,0.5), (0.5,0.5,0.5))]
@@ -53,28 +54,11 @@ print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
 import torch.nn as nn
 import torch.nn.functional as F
+from my_net import Net
 
-class Net(nn.Module):
-    def __init__(self):
-        N = 10
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, N, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(N, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-
+PATH = './cifar_net_trained.pth' # retrain
 net = Net()
+net.load_state_dict(torch.load(PATH))
 
 # Use a Classification Cross-Entropy loss and SGD with momentum.
 
@@ -83,7 +67,7 @@ import torch.optim as optim
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-for epoch in range(2):  # loop over the dataset multiple times
+for epoch in range(4):  # loop over the dataset multiple times
 
     running_loss = 0.0
     for i, data in enumerate(trainloader, 0):
