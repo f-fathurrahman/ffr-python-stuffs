91672240fa5dd4d168b9a7708f34247590f4b314
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-10-27 11:29:26 +0700

    Using torch.nn.Linear

--- pytorch/09_simple_nn.py
+++ pytorch/09_simple_nn.py
@@ -5,67 +5,79 @@ torch.manual_seed(1234) # for reproducibility
 #
 # Prepare data
 #
-t_c = [0.5, 14.0, 15.0, 28.0, 11.0, 8.0, 3.0, -4.0, 6.0, 13.0, 21.0]
-t_u = [35.7, 55.9, 58.2, 81.9, 56.3, 48.9, 33.9, 21.8, 48.4, 60.4, 68.4]
-t_c = torch.tensor(t_c).unsqueeze(1)
-t_u = torch.tensor(t_u).unsqueeze(1)
+x = torch.tensor([0.5, 14.0, 15.0, 28.0, 11.0, 8.0, 3.0, -4.0, 6.0, 13.0, 21.0])
+t = torch.tensor([35.7, 55.9, 58.2, 81.9, 56.3, 48.9, 33.9, 21.8, 48.4, 60.4, 68.4])
+# Append squeeze
+x = x.unsqueeze_(1)
+t = t.unsqueeze_(1)
 
 # Split into training and validation dataset
-Nsamples = t_u.shape[0]
+Nsamples = x.shape[0]
 Nval = int(0.2*Nsamples) # for validation data
 
 shuffled_indices = torch.randperm(Nsamples)
 train_indices = shuffled_indices[:-Nval]
 val_indices = shuffled_indices[-Nval:]
 
-train_t_u = t_u[train_indices]
-train_t_c = t_c[train_indices]
+x_train = x[train_indices]
+t_train = t[train_indices]
 
-val_t_u = t_u[val_indices]
-val_t_c = t_c[val_indices]
+x_val = x[val_indices]
+t_val = t[val_indices]
 
-# scale
-train_t_un = 0.1*train_t_u
-val_t_un = 0.1*val_t_u
+# scale or normalize feature
+x_train_n = 0.1*x_train
+x_val_n = 0.1*x_val
 
 #
 # Prepare model
 #
-linear_model = torch.nn.Linear(1,1)
-print( linear_model(val_t_un) )
 
-print( linear_model.weight )
-print( linear_model.bias )
+# Linear system:
+#
+# Args:
+#     in_features: size of each input sample
+#     out_features: size of each output sample
+#     bias: If set to ``False``, the layer will not learn an additive bias.
+#         Default: ``True``
 
-x = torch.ones(1)
-print( linear_model(x) )
+LinearModel = torch.nn.Linear(1,1)
+print("Test evaluating")
+print( LinearModel(x_val_n) )
 
-x = torch.ones(10, 1)
-print( linear_model(x) )
+print("\nLinearModel weight and bias:")
+print(LinearModel.weight)
+print(LinearModel.bias)
+
+print("\nUsing manual calculation:")
+print(LinearModel.weight*x_val_n[0] + LinearModel.bias)
+print(LinearModel.weight*x_val_n[1] + LinearModel.bias)
+
 
 optimizer = torch.optim.SGD(
-    linear_model.parameters(),
-    lr=1e-2
+    LinearModel.parameters(),
+    lr=1e-3
 )
 
 def training_loop(Nepochs, optimizer, model, loss_fn,
-                   train_t_u, val_t_u, train_t_c, val_t_c):
+                   x_train, t_train, x_val, t_val):
     for epoch in range(1,Nepochs+1):
         
-        train_t_p = model(train_t_u)
-        train_loss = loss_fn(train_t_p, train_t_c)
+        t_train_m = model(x_train)
+        loss_train = loss_fn(t_train_m, t_train)
 
-        val_t_p = model(val_t_u)
-        val_loss = loss_fn(val_t_p, val_t_c)
+        t_val_m = model(x_val)
+        loss_val = loss_fn(t_val_m, t_val)
 
         optimizer.zero_grad()
-        train_loss.backward()
+        loss_train.backward() # backprop on training data
         optimizer.step()
 
         if epoch == 1 or epoch % 100 == 0:
-            print(f"Epoch {epoch:8d}, Training loss {train_loss.item():10.4f},"
-                  f" Validation loss {val_loss.item():10.4f}")
+            print(f"Epoch {epoch:8d}, Training loss {loss_train.item():10.4f},"
+                  f" Validation loss {loss_val.item():10.4f}")
 
+# 
 def loss_fn(t_p, t_c):
     squared_diffs = (t_p - t_c)**2
     return squared_diffs.mean()
@@ -73,11 +85,41 @@ def loss_fn(t_p, t_c):
 training_loop(
     3000, 
     optimizer,
-    linear_model,
+    LinearModel,
     loss_fn,
-    train_t_un,
-    val_t_un,
-    train_t_c,
-    val_t_c
+    x_train,
+    t_train,
+    x_val,
+    t_val
 )
+
+# Model
+t_m = LinearModel(x)
+
+print("Loss on data: ", loss_fn(t_m, t))
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x_plt = x.numpy().squeeze()
+t_plt = t.numpy().squeeze()
+t_m_plt = t_m.detach().numpy().squeeze()
+
+print(x_plt.shape)
+print(t_plt.shape)
+print(t_m_plt.shape)
+
+
+plt.clf()
+idx_sorted = np.argsort(x_plt)
+x_plt = x_plt[idx_sorted]
+t_plt = t_plt[idx_sorted]
+t_m_plt = t_m_plt[idx_sorted]
+
+plt.plot(x_plt, t_plt, marker="x", label="data", linewidth=0)
+plt.plot(x_plt, t_m_plt, marker="o", label="model")
+plt.xlabel("Temperature (°Celsius)")
+plt.ylabel("Temperature (°Fahrenheit)")
+plt.legend()
+plt.savefig("IMG_09_simple_nn.pdf")
 
