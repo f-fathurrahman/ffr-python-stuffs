14dd5afbf7df058ef7082e5f3a415b64a3b2095e
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-08-20 22:03:20 +0700

    Experiment with font, test with color theme

--- pysimplegui/01_simple.py
+++ pysimplegui/01_simple.py
@@ -1,18 +1,21 @@
 import PySimpleGUI as sg
 
+sg.theme('DarkAmber')
+
 layout = [
-    [sg.Text("Some text of Row 1")],
-    [sg.Text("Enter something on Row 2"), sg.InputText()],
+    [sg.Text("Some text of Row 1", font="Arial 15")],
+    [sg.Text("Enter something on Row 2", font="Arial 15"), sg.InputText()],
     [sg.OK(), sg.Cancel()]
 ]
 
 window = sg.Window("Window Title", layout)
 
 while True:
-    event, values = window.Read()
-    if event in (None, "Cancel"):
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == "Cancel":
         print("Cancel is pressed.")
         print("I will break now.")
         break
+    print("You entered: ", values)
 
-window.Close()
+window.close()
